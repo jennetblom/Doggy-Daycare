99121a3d89195994c698cf04a1e0174554bf627b
Jennet Blom <jeboblom20@hotmail.com>
2024-09-19 14:42:34 +0200

    added welcomepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Welcome from './components/welcome';
+
 
 function App() {
   
+  
+
+  const [dogData, setDogData] = useState(null);
+  const [imageUrl, setImageUrl] = useState('');
+  const [count, setCount] = useState(0);
+  let apiUrl = 'https://api.jsonbin.io/v3/b/66ea6857e41b4d34e4325758';
+
+
+  useEffect(() => {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(dogData => setDogData(dogData.record));
+  },[]); 
 
   return (
     <div>
-      Hello
-    </div>
+      <Welcome />
+      
+
+        {/* {dogData ? (
+          <div>
+          <p>Hello {dogData[count].name}</p>
+          
+          <button onClick={() => setCount(count + 1)}>
+            <img src={dogData[count].img} />
+            </button>
+          </div>
+        ) : (
+          'Loading'
+        )
+      } */}
+     </div>
+  
+  
   )
 }
 
---
+++ src/components/welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Welcome = () => {
+    return (
+        <section>
+            <img src="https://db3pap003files.storage.live.com/y4mRRCmW9RBxpXVLBj-6cfAIngMxV1b6P7Y_DBKF3c449rPF1mKreMu4EIP-lKmWym6Cmvzba1bSvHEB7wg2QJxQT1OpVsU4NYRiRVLRPQK_9n2gsv8imVavYGOHoMh0AZHoxgO8VjBNCAmKwa0YANdxdIDfqeZFGrzf2Gh7hvyruDBVwgeiSQxdWktJZCSOYCMTZ-bbajQqPBpp_E1Cwa-WA?encodeFailures=1&width=360&height=360" />
+            <h2>Welcome doglovers!</h2>
+            <h2>Here at Doggy daycare, we keep track of all the dogs that visits us!  😃</h2>
+            <button>See the dogs</button>
+        </section>
+    )
+}
+
+export default Welcome
